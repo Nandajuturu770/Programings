f5c0814514ffe923afa533fb337a81109d784fd6
Nandakiran <nandajuturu@gmail.com>
2025-12-22 11:43:27 +0530

    case convertion class has been implemented

---
+++ tasks/src/string_programs/CaseConvertion.java
@@ -0,0 +1,50 @@
+package string_programs;
+
+import java.util.Scanner;
+
+public class CaseConvertion {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter your word to change upper case :: ");
+		String string = scanner.nextLine();
+		System.out.println("Upper Case :: "+toChangeUpperCase(string));
+		System.err.println("Lower Case :: "+toChangeLowerCase(string));
+	}
+
+	/**
+	 * This method is used to change upper case.
+	 * @param string <code>String<code>
+	 */
+	public static String toChangeUpperCase(String string){
+		StringBuilder upperCase = new StringBuilder();
+		for (char ch : string.toCharArray()) {
+			if(ch >= 'a' && ch <= 'z'){
+				ch -= 32;
+			}
+				upperCase.append(ch);
+		}
+		return upperCase.toString();
+	}
+
+	/**
+	 * This method is used to change lower case.
+	 * @param string <code>String<code>
+	 */
+	public static String toChangeLowerCase(String string){
+		StringBuilder upperCase = new StringBuilder();
+		for (char ch : string.toCharArray()) {
+			if(ch >= 'A' && ch <= 'Z'){
+				ch += 32;
+			}
+				upperCase.append(ch);
+		}
+		return upperCase.toString();
+	}
+}
+
+/*
+Please enter your word to change upper case :: Java Program Language
+Upper Case :: JAVA PROGRAM LANGUAGE
+Lower Case :: java program language
+ */
--- tasks/src/string_programs/TOUpperCase.java
+++
@@ -1,9 +0,0 @@
-package string_programs;
-
-public class TOUpperCase {
-	String name = "Nandakiran jurutu";
-	public static void main(String[] args) {
-		TOUpperCase s = new TOUpperCase();
-		System.out.println(s);
-	}
-}
